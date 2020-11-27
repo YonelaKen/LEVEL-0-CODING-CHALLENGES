2b6d5c0b3501ba0241fd24c2bfeac59325fef32c
YonelaKen <74543272+YonelaKen@users.noreply.github.com>
2020-11-27 12:42:47 +0200

    Task 1.js

--- Task 1.js
+++ Task 1.js
@@ -1,8 +1,12 @@
+//Declaration of the variables
 var x = 0;
 var y = 1;
+//Printing the x and y variables before calculations
 console.log(x);
 console.log(y);
+//Performing calculation with x and y
 x = x + 3;
 y = y + x;
+//Printing x and y after performing calculations
 console.log(x);
 console.log(y);
