370459ff95bce50e497fd8bf0f4e7f5b26aa730f
YonelaKen <74543272+YonelaKen@users.noreply.github.com>
2020-12-01 12:24:05 +0200

    Add files via upload

---
+++ Task 5.js
@@ -0,0 +1,7 @@
+function calculateArea(a,b,c){
+    //This line is the sum that defines the calculation of the area
+    var area = 0.5 * (a + b + c);
+    //This funtion returns the value of the area
+    return area;
+  }
+  console.log(calculateArea(3,4,5));
