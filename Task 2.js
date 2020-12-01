5319b47f5d091e97b94c94dab6acc6e88b83ca1b
YonelaKen <74543272+YonelaKen@users.noreply.github.com>
2020-12-01 11:31:39 +0200

    Add files via upload

---
+++ Task 2.js
@@ -0,0 +1,14 @@
+//This line of code is the declaration of the variables
+var x,y,z,a,b;
+//These lines of code assigns values to the variables.
+x = 1 + 1 * 2
+y = (1 + 1) * 2
+z = 1 + ( 1 * 2 )
+a =  1 + 1 * 2 / 2
+b =  (1 + 1 * 2 ) /  2
+//This prints out then values of variables to check if the variables are correctly calculated.
+console.log("The value of x is " + x);
+console.log("The value of y is " + y);
+console.log("The value of z is " + z);
+console.log("The value of a is " + a);
+console.log("The value of b is " + b);
