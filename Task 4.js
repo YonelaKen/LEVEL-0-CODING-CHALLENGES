7603237a2308d84fc3ad9c65f0899bbc6036cc28
YonelaKen <74543272+YonelaKen@users.noreply.github.com>
2020-12-09 10:09:24 +0200

    Update Task 4.js

--- Task 4.js
+++ Task 4.js
@@ -1,13 +1,12 @@
 //This fuction accepts to variables and checks the values to see if the values of the numbers is 3, OR if the sum of the numbers is 3 then it will return true. Otherwise it will return false.
 function checkValue(a,b) {
 
-    //This the declation of the sum variable as the sum variable is not global and also so we can get to check if the sum is equal to 3
     var sum = a + b;
     //This if statement checks if a OR b OR the sum of both numbers is equal to 65 and will print true
     if(a === 3 || b === 3 || sum === 3){
       return true;
     }
-    //If the a OR b OR the sum of both numbers is not equal to 3 and will print out false
+    
     return false; 
   
   }
